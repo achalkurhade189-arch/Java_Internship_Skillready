e6914e0d639f5290f0290db73a811afea4798565
achalkurhade189-arch <achalkurhade189@gmail.com>
2026-06-23 16:09:23 +0530

    Add files via upload

---
+++ BitwiseOp.java
@@ -0,0 +1,10 @@
+public class BitwiseOp 
+{
+    public static void main(String args[]){
+        int a = 5;
+        int b = 3;
+        System.out.println("bitwise AND op = "+(a&b));
+        System.out.println("bitwise OR op = "+(a|b));
+        System.out.println("bitwise XOR op = "+(a^b));
+    }
+}
