51c2b8825ce566906da507567560c0dc113d3919
achalkurhade189-arch <achalkurhade189@gmail.com>
2026-06-29 19:58:21 +0530

    Add files via upload

---
+++ foreach_loop.java
@@ -0,0 +1,12 @@
+package ControlFlow_Statement.Looping_statements;
+
+public class foreach_loop{
+    public static void main(String[] args) {
+        int a[] = {10, 20, 30, 40, 50};
+
+        for(int number:a)
+        {
+            System.out.print(number+" ");
+        }
+    }
+}
